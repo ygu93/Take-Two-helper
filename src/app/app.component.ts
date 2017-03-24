a251b4be593659e5df810416b36ee6da5fb6d903
Yue Gu <yuegu1993@gmail.com>
2017-03-24 01:32:56 -0700

    add http service for json files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
 import { Component } from '@angular/core';
+import { CardDataService } from 'app/card-data.service';
 
 @Component({
   selector: 'app-root',
+  providers: [CardDataService],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'app works!';
+  class;
+
+  constructor(private cardDataService: CardDataService) {
+
+  }
+
+  getBlood() {
+    this.class = this.cardDataService.getBlood();
+  }
+
+  getDragon() {
+    this.class = this.cardDataService.getDragon();
+  }
+
+  getForest() {
+    this.class = this.cardDataService.getForest();
+  }
+
+  getHaven() {
+    this.class = this.cardDataService.getHaven();
+  }
+
+  getRune() {
+    this.class = this.cardDataService.getRune();
+  }
+
+  getShadow() {
+    this.class = this.cardDataService.getShadow();
+  }
+
+  getSword() {
+    this.class = this.cardDataService.getSword();
+  }
+
 }
--- src/app/card-data.service.ts
+++ src/app/card-data.service.ts
@@ -1,8 +1,41 @@
 import { Injectable } from '@angular/core';
+import {Http, Response, Headers, RequestOptions} from '@angular/http';
+import {Observable} from 'rxjs/Rx';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class CardDataService {
 
-  constructor() { }
+  constructor(private http: Http) {
 
+  }
+
+
+  getBlood(): Observable<any> {
+    return this.http.get('./resources/cards/Blood.json').map((res: any) => res.json());
+  }
+
+  getDragon() {
+    return this.http.get('./resources/cards/Dragon.json').map((res: any) => res.json());
+  }
+
+  getForest() {
+    return this.http.get('./resources/cards/Forest.json').map((res: any) => res.json());
+  }
+
+  getHaven() {
+    return this.http.get('./resources/cards/Haven.json').map((res: any) => res.json());
+  }
+
+  getRune() {
+    return this.http.get('./resources/cards/Rune.json').map((res: any) => res.json());
+  }
+
+  getShadow() {
+    return this.http.get('./resources/cards/Shadow.json').map((res: any) => res.json());
+  }
+
+  getSword() {
+    return this.http.get('./resources/cards/Sword.json').map((res: any) => res.json());
+  }
 }
