5db7b6cd60274f4ecf78103a4899305fa4a6e367
Yue Gu <yuegu1993@gmail.com>
2017-05-14 02:50:48 -0700

    reformat searchbox to use ngmodel

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 export class SearchComponent implements OnInit {
 
   @Input() class: any;
-  query;
+  query = '';
   result;
   cardNames;
   constructor() { }
